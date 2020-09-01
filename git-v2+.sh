d2cf3606e5ac888c22070d30945d9b7898a0c0c4
Erik Dubois <erik.dubois@gmail.com>
2020-09-01 08:14:35 +0200

    monthly cleanup

---
+++ git-v2+.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# checking if I have the latest files from github
+echo "Checking for newer files online first"
+git pull
+
+cd x86_64
+sh updaterepo.sh
+
+git add --all .
+
+# Give a comment to the commit if you want
+echo "####################################"
+echo "Write your commit comment!"
+echo "####################################"
+
+#read input
+input="update"
+# Committing to the local repository with a message containing the commit text
+
+git commit -m "$input"
+
+# Push the local files to github
+
+git push -u origin master
+
+
+echo "################################################################"
+echo "###################    Git Push Done      ######################"
+echo "################################################################"
